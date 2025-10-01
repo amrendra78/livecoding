1169aab4f68fcb124d17386cf809736fe7dd22cf
Amrendra <hellowordls078.com>
2025-10-01 16:56:06 +0530

    Ready-to-deploy Angular SSR + Vercel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,6 @@ export const routes: Routes = [
   {
     path: 'editor/:roomId',
     component: EditorComponent,
-    data: { renderMode: 'ssr' } // SSR mode enable
+    data: { renderMode: 'ssr' } // SSR mode for dynamic route
   },
 ];
